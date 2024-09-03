fe91177101722cd270c542f8b8b168c639a88b79
Shahmar-Kazimov <shahmar.kazimoff@gmail.com>
2024-09-03 17:51:25 +0400

    validator

--- shahmar-eclipse-master/frontend/src/components/checkout/checkout.jsx
+++ shahmar-eclipse-master/frontend/src/components/checkout/checkout.jsx
@@ -1,4 +1,3 @@
-import { log } from 'har-validator';
 import { useEffect, useState } from 'react';
 import { useSelector } from "react-redux";
 import { ToastContainer, toast } from 'react-toastify';
